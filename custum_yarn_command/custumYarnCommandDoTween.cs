d7e4b0c5a0a9ad705e5d82b484c1afed562ec0b7
jang-chinseok <jcs000729@naver.com>
2023-01-23 19:51:31 +0900

    dotween Test

---
+++ custum_yarn_command/custumYarnCommandDoTween.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class custumYarnCommandDoTween : MonoBehaviour
+{
+
+    public DialogueRunner DR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DR.AddCommandHandler<GameObject,float,float,float,float>("move", move);
+        DR.AddCommandHandler<GameObject,float,float,float,float>("rotate", rotate);
+        // DR.AddCommandHandler<string,string>("obejctActive", obejctActive);
+        DR.AddCommandHandler<GameObject,float,float>("fade", fade);
+        DR.AddCommandHandler<GameObject,Color,float>("changeColor", changeColor);
+        // DR.AddCommandHandler<string,float,float>("moveDown", moveDown);
+        // DR.AddCommandHandler<string,float,float,float>("shakeHorizontal", shakeHorizontal);
+        // DR.AddCommandHandler<string, float>("testAnimation", testAnimation);
+    }
+
+    // Update is called once per frame
+    void move(GameObject objectName,float xPosition,float yPosition,float zPosition,float moveSpeed){
+        objectName.transform.DOMove(new Vector3(xPosition, yPosition,zPosition), moveSpeed);
+    }
+    void rotate(GameObject objectName,float xAngle,float yAngle,float zAngle,float rotateSpeed){
+        objectName.transform.DORotate(new Vector3(xAngle, yAngle,zAngle), rotateSpeed);
+    }
+    void changeColor (GameObject objectName,Color color,float changeSpeed){
+        SpriteRenderer objectColor = objectName.GetComponent<SpriteRenderer>();
+        objectColor.DOColor(color, changeSpeed);
+    }
+    void fade (GameObject objectName,float alpah,float fadeSpeed){
+        SpriteRenderer objectColor = objectName.GetComponent<SpriteRenderer>();
+        objectColor.DOFade(alpah, fadeSpeed);
+    }
+
+
+
+
+
+    
+    
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- custum_yarn_command/custumYarnCommands.cs
+++ custum_yarn_command/custumYarnCommands.cs
@@ -15,9 +15,10 @@ private void Awake() {
         DR.AddCommandHandler<string,float,float,float,float>("moveIlustration", moveIlustration);
         DR.AddCommandHandler<string,float,float,float,float>("rotateIlustration", rotateIlustration);
         DR.AddCommandHandler<string,string>("obejctActive", obejctActive);
-        DR.AddCommandHandler<string,float,float>("fade", fade);
+        // DR.AddCommandHandler<string,float,float>("fade", fade);
         DR.AddCommandHandler<string,float,float>("moveDown", moveDown);
         DR.AddCommandHandler<string,float,float,float>("shakeHorizontal", shakeHorizontal);
+        DR.AddCommandHandler<string, float>("testAnimation", testAnimation);
 
 }               
 // =========== yet making object Instantiate createing ===============
@@ -247,6 +248,45 @@ IEnumerator shakeHorizontalReverse()
     StopCoroutine("moveDownReverse");
     deltaTime = 0;
     
+}
+
+void testAnimation(string ilustration, float moveTimeTotal)
+{
+    float moveScale = 100; // 상하 이동 범위 외부에서 수정 가능한 테스트 숫자. 조정후 fix된 값이 생기면 그 값으로 고정 예정.
+    float moveTime = 3; // 상하 이동 속도
+    Vector3 startLocation= this.transform.position; // 오브젝트의 이동 시작 위치
+    bool moveDiraction = true; // 오브젝트가 움직이는 방향
+    Vector3 moveDir = new Vector3(this.transform.position[0],this.transform.position[1]+moveScale, this.transform.position[2]);  // 오브젝트가 이동할 좌표
+
+    float DeltaTime = 0;
+    while(DeltaTime<moveTimeTotal)
+    {
+        Vector3 vector3Now =  this.transform.position; // 오브젝트의 현재 위치 업데이트
+        if (moveDiraction == true)
+        { // 정방향이동
+            DeltaTime += Time.deltaTime;
+            this.transform.position = Vector3.Lerp (startLocation, moveDir, DeltaTime/moveTime);
+            
+            if(DeltaTime>moveTime){ //거의 다 도착하면 역방향이동으로 전환
+                DeltaTime = 0;
+                Vector3 nextMovePosition = startLocation;
+                startLocation = moveDir;
+                moveDir = nextMovePosition;
+                moveDiraction = false;    //거의 다 도착하면 역방향이동으로 전환
+            }
+        }
+        else { // 역방향 이동.
+            DeltaTime += Time.deltaTime;
+            this.transform.position = Vector3.Lerp (startLocation, moveDir, DeltaTime/moveTime);
+            if(DeltaTime>moveTime){  //거의 다 도착하면 정방향 이동으로 전환
+                DeltaTime =0;
+                Vector3 nextMovePosition = startLocation;
+                startLocation = moveDir;
+                moveDir = nextMovePosition;
+                moveDiraction = true;  // 정방향이동
+            }
+        }
+        Debug.Log($"{moveDiraction}현재 움직이는 방향");}
 }
 
 
